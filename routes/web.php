a978bc69fe0f78b13e06c60198bfae081709d2e1
vilfanvenci <vilfanvenci@gmail.com>
2023-02-12 21:56:04 +0700

    auth-register blade template

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages.auth-login');
 });
+
+Route::get('register', function () {
+    return view('pages.auth-register');
+});
