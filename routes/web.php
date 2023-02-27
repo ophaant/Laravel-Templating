550d0a7c2075a01d2cff9f237e882269455dc49a
ophaant <vilfanvenci@gmail.com>
2023-02-27 21:24:50 +0700

    dashboard & using vite

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('register', function () {
     return view('pages.auth-register');
 });
+
+Route::get('dashboard', function () {
+    return view('pages.dashboard',['type_menu' => 'dashboard']);
+});
